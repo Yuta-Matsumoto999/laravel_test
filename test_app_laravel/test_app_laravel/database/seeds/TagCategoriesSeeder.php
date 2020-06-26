f5546cdd17f051925f921821cd116801a8c06287
松本雄大 <y.matsumoto0723@gmail.com>
2020-06-26 18:36:24 +0900

    必要ファイルの用意

---
+++ test_app_laravel/test_app_laravel/database/seeds/TagCategoriesSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagCategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
--- test_app_laravel/test_app_laravel/routes/web.php
+++ test_app_laravel/test_app_laravel/routes/web.php
@@ -20,3 +20,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('question/mypage', 'QuestionController@showMypage')->name('question.showMypage');
+Route::resource('question', 'QuestionController');
+Route::post('question/createConfirm', 'QuestionController@createConfirm')->name('question.createConfirm');
+Route::post('question/comment/{id}', 'QuestionController@commentCreate')->name('question.comment');
+Route::post('question/updateConfirm/{id}', 'QuestionController@updateConfirm')->name('question.updateConfirm');
+
+Route::get('/login/twitter', 'Auth\SocialController@getTwitterAuth');
+Route::get('/login/twitter/callback', 'Auth\SocialController@etTwitterAuthCallback');
